99450b9d56137a71a3521c2909e2a7f2f4cdcfcd
jingzhou <jingzhou_wang@hotmail.com>
2025-06-08 18:39:37 +0800

    MainActivity

--- app/src/main/java/com/illiad/troad/MainActivity.kt
+++ app/src/main/java/com/illiad/troad/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.illiad.troad
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.core.content.ContextCompat
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,4 +30,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private val vpnStatusReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == Consts.ACTION_VPN_STATUS_BROADCAST) {
+                val message = intent.getStringExtra(Consts.EXTRA_STATUS_MESSAGE)
+                val isConnected = intent.getBooleanExtra(Consts.EXTRA_IS_CONNECTED, false)
+
+                // Update your UI here based on the message and isConnected state
+                Log.d("MyActivity", "VPN Status Received: $message, Connected: $isConnected")
+                // e.g., myStatusTextView.text = message
+                // e.g., myConnectButton.isEnabled = !isConnected
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter(Consts.ACTION_VPN_STATUS_BROADCAST)
+        // If using system-wide sendBroadcast in the service:
+        ContextCompat.registerReceiver(
+            this, // Context
+            vpnStatusReceiver,
+            intentFilter,
+            ContextCompat.RECEIVER_EXPORTED // Specify exported
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // If using system-wide:
+        unregisterReceiver(vpnStatusReceiver)
+    }
+
 }
