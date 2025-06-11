d35a6ea70ae7488af6bc834847ebeee8c3d79494
jingzhou <jingzhou_wang@hotmail.com>
2025-06-11 18:08:22 +0800

    Utils as singleton

--- app/src/main/java/com/illiad/troad/handler/Utils.kt
+++ app/src/main/java/com/illiad/troad/handler/Utils.kt
@@ -4,15 +4,14 @@ import io.netty.buffer.Unpooled
 import io.netty.channel.Channel
 import io.netty.channel.ChannelFutureListener
 
-class Utils {
+object Utils {
     /**
      * Closes the specified channel after all queued write requests are flushed.
      */
-    companion object {
-        fun closeOnFlush(ch: Channel) {
-            if (ch.isActive()) {
-                ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE)
-            }
+
+    fun closeOnFlush(ch: Channel) {
+        if (ch.isActive()) {
+            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE)
         }
     }
 }
