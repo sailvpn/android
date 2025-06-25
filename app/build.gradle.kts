eb2934d85bd98c567a49d8873be7fe74c1e0d8a5
jingzhou <jingzhou_wang@hotmail.com>
2025-06-25 21:08:28 +0800

    build.gradle.kts: packing.resources.excludes ..

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,13 @@ android {
     buildFeatures {
         compose = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/INDEX.LIST"
+            excludes += "/META-INF/io.netty.versions.properties"
+        }
+
+    }
 }
 
 dependencies {
