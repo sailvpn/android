7c54b79b125f7884c6676efa9a3d266cbf9c5691
jingzhou <jingzhou_wang@hotmail.com>
2025-07-02 15:04:19 +0800

    merge Rss into Utils.kt

--- app/src/main/java/com/illiad/troad/service/Utils.kt
+++ app/src/main/java/com/illiad/troad/service/Utils.kt
@@ -1,12 +1,12 @@
 package com.illiad.troad.service
 
+import android.content.res.Resources
 import android.os.ParcelFileDescriptor
 import io.netty.buffer.Unpooled
 import io.netty.channel.Channel
 import io.netty.channel.ChannelFutureListener
 import java.nio.channels.FileChannel
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.ReentrantLock
 
@@ -40,6 +40,17 @@ object Utils {
     var serverDomain: String = "127.0.0.1"
     var serverPort: Int = 2080
     var sharedSecret: String = "sharedSecret"
+
+    // resources getters
+    private val rss: Resources = Resources.getSystem()
+
+    fun getString(id: Int): String {
+        return rss.getString(id)
+    }
+
+    fun getInt(id: Int): Int {
+        return rss.getInteger(id)
+    }
 
     /**
      * Closes the specified channel after all queued write requests are flushed.
