b1533fc062fe261d19310f6f3985a47b45d113b4
jingzhou <jingzhou_wang@hotmail.com>
2025-07-12 16:03:56 +0800

    build.gradle, libs.versions: dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -38,6 +40,7 @@ android {
         compose = true
     }
     packaging {
+
         resources {
             excludes += "/META-INF/INDEX.LIST"
             excludes += "/META-INF/io.netty.versions.properties"
