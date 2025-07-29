e88e1b471f501b22955ff84b1a6c0f0e184e7229
jingzhou <jingzhou_wang@hotmail.com>
2025-07-29 13:08:57 +0800

    DemuxHandler.kt: detect and omit ICMP packets

--- app/src/main/java/com/illiad/troad/service/handler/ip/DemuxHandler.kt
+++ app/src/main/java/com/illiad/troad/service/handler/ip/DemuxHandler.kt
@@ -6,6 +6,10 @@ import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
 import org.pcap4j.packet.IpPacket
+import org.pcap4j.packet.IpV4Packet
+import org.pcap4j.packet.IpV6ExtHopByHopOptionsPacket
+import org.pcap4j.packet.IpV6Packet
+import org.pcap4j.packet.namednumber.IpNumber
 
 @ChannelHandler.Sharable
 object DemuxHandler : SimpleChannelInboundHandler<MutableList<IpPacket?>?>() {
@@ -17,7 +21,34 @@ object DemuxHandler : SimpleChannelInboundHandler<MutableList<IpPacket?>?>() {
         for (packet in packets) {
             if (packet == null) {
                 continue
+            } else if (packet is IpV4Packet) {
+                val ipV4Packet = packet as IpV4Packet
+                if (ipV4Packet.header.protocol == IpNumber.ICMPV4) {
+                    // This is an ICMPv4 packet
+                    continue
+                    // You can then get the ICMPv4 packet if needed:
+                    // val icmpV4Packet = ipV4Packet.payload as IcmpV4CommonPacket
+
+                }
+            } else if (packet is IpV6Packet) {
+                val ipV6Packet = packet as IpV6Packet
+                // check if the packet is an ICMPv6 packet
+                if (ipV6Packet.header.nextHeader == IpNumber.ICMPV6) {
+                    // This is an ICMPv6 packet
+                    continue
+                    // You can then get the ICMPv6 packet if needed:
+                    // val icmpV6Packet = ipV6Packet.payload as IcmpV6CommonPacket
+                    // ICMPV6 can also be indicvated by Hop-by-Hop option header first
+                } else if (ipV6Packet.header.nextHeader == IpNumber.IPV6_HOPOPT) {
+                    val hopByHopPacket = ipV6Packet.payload
+                    if (hopByHopPacket is IpV6ExtHopByHopOptionsPacket) {
+                        if (hopByHopPacket.header.nextHeader == IpNumber.ICMPV6) {
+                            continue
+                        }
+                    }
+                }
             }
+
             val connection = Connection.extractConnetion(packet)
             if (connection != null) {
                 var session = Demux.getSession(connection)
