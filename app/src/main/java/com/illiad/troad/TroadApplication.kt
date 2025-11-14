26d61266d34aa655ceb7c6ce0a5db8738145973c
jingzhou <jingzhou_wang@hotmail.com>
2025-11-14 18:00:35 +0800

    TroadApplication, AndroidManifest: MyApplication --> TroadApplication

--- app/src/main/java/com/illiad/troad/TroadApplication.kt
+++ app/src/main/java/com/illiad/troad/TroadApplication.kt
@@ -5,7 +5,7 @@ import android.content.ComponentCallbacks2
 import android.content.res.Configuration
 import android.util.Log
 
-class MyApplication : Application(), ComponentCallbacks2 {
+class TroadApplication : Application(), ComponentCallbacks2 {
 
     override fun onCreate() {
         super.onCreate()
@@ -19,25 +19,25 @@ class MyApplication : Application(), ComponentCallbacks2 {
     override fun onTrimMemory(level: Int) {
         super.onTrimMemory(level) // It's good practice to call super
 
-        Log.i("MyApplication", "onTrimMemory() called with level: $level")
+        Log.i("TroadApplication", "onTrimMemory() called with level: $level")
 
         if (level >= ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW) {
             // System is running low on memory, aggressively release caches
             // MyImageCache.getInstance().clearMemory()
-            Log.w("MyApplication", "Aggressively trimming memory for RUNNING_LOW or worse.")
+            Log.w("TroadApplication", "Aggressively trimming memory for RUNNING_LOW or worse.")
         }
 
         if (level >= ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
             // UI is no longer visible, release UI-specific resources
             // MyUiResourceHolder.clearResources()
-            Log.i("MyApplication", "Trimming memory because UI is hidden.")
+            Log.i("TroadApplication", "Trimming memory because UI is hidden.")
         }
 
         if (level == ComponentCallbacks2.TRIM_MEMORY_COMPLETE || level == ComponentCallbacks2.TRIM_MEMORY_MODERATE) {
             // App is a good candidate for killing if more memory is needed
             // Release almost everything that's not critical
             Log.w(
-                "MyApplication",
+                "TroadApplication",
                 "Trimming memory with COMPLETE or MODERATE. Releasing almost everything."
             )
         }
@@ -51,7 +51,7 @@ class MyApplication : Application(), ComponentCallbacks2 {
         super.onLowMemory()
         // This is an older, more drastic callback.
         // onTrimMemory with TRIM_MEMORY_COMPLETE is roughly equivalent.
-        Log.e("MyApplication", "onLowMemory() called. This is serious!")
+        Log.e("TroadApplication", "onLowMemory() called. This is serious!")
         // Release as many resources as possible.
     }
 }
