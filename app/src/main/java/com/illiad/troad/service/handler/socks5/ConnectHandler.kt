021c28f0bb50596753e1cdc2f2633b9805203e2b
jingzhou <jingzhou_wang@hotmail.com>
2025-11-21 01:23:46 +0800

    ConnectHandler: minor change

--- app/src/main/java/com/illiad/troad/service/handler/socks5/ConnectHandler.kt
+++ app/src/main/java/com/illiad/troad/service/handler/socks5/ConnectHandler.kt
@@ -1,7 +1,6 @@
 package com.illiad.troad.service.handler.socks5
 
 import com.illiad.troad.service.HandlerNamer
-import com.illiad.troad.service.Utils.closeOnFlush
 import com.illiad.troad.service.codec.socks5.V5ClientDecoder
 import com.illiad.troad.service.codec.socks5.V5ClientEncoder
 import com.illiad.troad.service.handler.ip.Connection
