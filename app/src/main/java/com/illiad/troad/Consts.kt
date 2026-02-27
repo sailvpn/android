874fb4f9d704838a0e8e9d2377d0f3c9acf5ec34
wjz <jingzhou_wang@hotmail.com>
2026-02-27 23:00:47 +0800

    Consts: rm TUN_IP

--- app/src/main/java/com/illiad/troad/Consts.kt
+++ app/src/main/java/com/illiad/troad/Consts.kt
@@ -7,7 +7,6 @@ object Consts {
     const val DH = "DtlsHandler"
     const val VM = "SettingsViewModel"
 
-    const val TUN_IP = "10.8.0.2"
     const val DNS1 = "8.8.8.8"
     const val DNS2 = "8.8.4.4"
     const val MTU = 1300 // Android VpnService.Builder.establish() will crash on a too high number
